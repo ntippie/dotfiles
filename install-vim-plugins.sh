cf7dc6b26fad9617a76828949bcc35834cfbead2
Nate Tippie <nate@tippie.com>
2019-01-16 01:51:07 -0800

    Add support for WSL clipboard, add gitgutter vim plugin.

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -7,3 +7,7 @@ mkdir -p .vim/bundle
 cd .vim/bundle && \
   git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 cd -
+
+cd .vim/bundle && \
+  git clone git://github.com/airblade/vim-gitgutter.git
+cd -
