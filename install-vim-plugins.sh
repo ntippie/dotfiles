40b0652d0ad408126f27438e5f3923a376b3aaa2
Nate Tippie <nate@tippie.com>
2021-10-27 13:32:50 -0700

    Remove pathogen.

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-mkdir -p .vim/autoload
-curl -LSso .vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-
 mkdir -p .vim/bundle
 cd .vim/bundle && \
   git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
