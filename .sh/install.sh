6a747046c753c5244cebb0a1ddd777b06760aef2
Nate Tippie <ntippie@users.noreply.github.com>
2022-09-28 22:15:26 -0700

    Update install.sh

--- .sh/install.sh
+++ .sh/install.sh
@@ -3,3 +3,10 @@ sudo apt-get install -y $(cat Aptfile)
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 source ~/.zshrc
 nvm install && nvm use && nvm alias default $(cat ../.nvmrc)
+if [[ -z "$TMUX" && -z "$VSCODE_INSPECTOR_OPTIONS" ]]; then
+  tmux attach || tmux new
+fi
+
+if grep -qi Microsoft /proc/version; then
+  sudo ln -s $(which explorer.exe) /usr/local/bin/xdg-open
+fi
