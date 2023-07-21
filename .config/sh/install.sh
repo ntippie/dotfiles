225c05d63eefa4cf69f3e4f23e13290c8c20da8c
Nate Tippie <nate@tippie.com>
2023-07-21 12:04:02 -0700

    2023-07-21T12:04:02-07:00 -- red-tiger.local

--- .config/sh/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get install -y $(cat Aptfile)
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
-source ~/.zshrc
-nvm install && nvm use && nvm alias default $(cat ~/.nvmrc)
-
